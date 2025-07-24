aecfdc5175f435992186a320f9577dc98939e6cd
Alexander Doudkin <alexander@ikaros.ventures>
2025-07-24 20:44:28 +0200

    feat: create legal content page layout
    - Added a new Vue component for the legal content page with a defined layout.
    - Implemented styles for legal content, enhancing typography and readability.
    - Integrated a ContentDoc component for dynamic content rendering.

---
+++ pages/hfbk/[...slug].vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+
+definePageMeta({
+    layout: 'legal',
+});
+</script>
+
+<template>
+  <main>
+    <ContentDoc />
+  </main>
+</template>
+
+<style>
+.legal-content * {
+  font-size: 1.125rem; /* text-lg */
+  line-height: 1.75rem; /* leading-7 */
+}
+
+.legal-content h1,
+.legal-content h2,
+.legal-content h3,
+.legal-content h4,
+.legal-content h5,
+.legal-content h6 {
+  font-weight: 700; /* bold */
+  margin-top: 1.5em;
+  margin-bottom: 1em;
+}
+</style>
